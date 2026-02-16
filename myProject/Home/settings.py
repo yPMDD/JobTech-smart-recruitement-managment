7846d1bb2499fe0ba0ba10876c6ea6225e9c7a21
yPMDD <saadaithamida.1@gmail.com>
2026-02-16 18:06:03 +0000

    Debug: Enable console logging for 500 errors

--- myProject/Home/settings.py
+++ myProject/Home/settings.py
@@ -108,3 +108,25 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Logging Configuration to print errors to console in production
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
